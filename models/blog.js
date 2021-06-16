9e4cb346554e513409eab6f8b5c42819b608f3b7
njsakn <85639462+njsakn@users.noreply.github.com>
2021-06-16 13:30:17 +0200

    Update blog.js

--- models/blog.js
+++ models/blog.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
  
 const blogSchema = new Schema({
-    
-    testo:{
+    title:{
         type: String,
-        required: true   
+        required: true
     },
-    body:{
+    testo:{
         type: String,
         required: true   
     }
